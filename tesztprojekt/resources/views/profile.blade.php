fc361ff074c2406ad7d6d8d0c55b2468758cdcea
kilianmarcell <kilian.marcell.andras@gmail.com>
2021-12-15 14:03:36 +0100

    Kommandok hozzáadva

--- tesztprojekt/resources/views/profile.blade.php
+++ tesztprojekt/resources/views/profile.blade.php
@@ -1,13 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Profil</title>
-</head>
-<body>
-    <h1> {{ $user->name }} </h1>
-    <p>Email: {{ $user->email }} </p>
-</body>
-</html>
+@extends('layouts.app')
+
+@section('title', 'User profile')
+
+@section('content')
+<h1>{{ $user->name }}</h1>
+<p>Email: {{ $user->email }} </p>
+@endsection
